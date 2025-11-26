8973a81637310c8173ae35bcff33ab356d311bb3
NikhilPaliwal11 <nikhilpaliwal97@gmail.com>
2025-11-26 12:40:34 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,10 @@ from src.logger import logging
 import pandas as pd
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTranformationConfig
 
+# All the inputs required for DataIngestion component
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -15,8 +18,9 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()   # Now injestion config contains all folder name which we define in DataInjestioncofig class.     
 
+    #This function is used to save the excel file in form of raw data ,train and test. 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
@@ -37,10 +41,13 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,self.ingestion_config.test_data_path
             )
-        
+            # We return train and test data which can be used in data transformation
         except Exception as e:
             raise CustomException(e,sys)
             
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,129 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+from src.utils import save_object
+
+
+@dataclass
+class DataTranformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTranformationConfig()           #Initalizing
+     
+     # this function is used to create a pickle file which is responsible for converting categorical into numerical and to perform statndard scaler
+    def get_data_transformer_object(self):
+        '''This function is responsible for data transformation
+        '''
+        try:
+            numerical_columns = ["writing_score","reading_score"]
+            categorical_columns = ["gender","race_ethnicity","parental_level_of_education","lunch","test_preparation_course"]
+
+            # Create a pipeline and we need to handle missing values too
+
+            num_pipeline = Pipeline(
+            steps = [("imputer",SimpleImputer(strategy="median")),
+                     ("scaler",StandardScaler())]
+            # We handling missing value and use median because there are outlier in data.
+            # We do standard scaler for numerical variables.
+            )
+
+            # Categorical pipleine handling Simple Imputer for missing values , one hot encoder for encoding because of less categories in each variable.
+            # Standardscaler for converting it standard normal varibale between 0 and 1
+            cat_pipeline = Pipeline(
+                steps = [
+                    ("imputer",SimpleImputer(strategy= "most_frequent")),    #mode
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+            )
+            #Dont subtract ny mean Just scale by standard deviation and keep zeros as zeors because sparse matrix contains [0,0,0,1,0] one hot encoding
+            # if mean 0.05 all zero would become non - zeros , your sparse matrix become dense huge memory problem.
+            # Scikit-learn prevented your sparse data from getting converted into a massive dense matrix and eating your memory.
+            logging.info(f"Categorical columns:{categorical_columns}")
+            logging.info(f"Numerical columns:{numerical_columns}")
+        
+            # Now we combine both numerical pipeline and categorical pipeline together and for that we use preprocessing.
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+        # Starting our data transformation inside this function
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            # We first read our train and test data
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessor object")
+
+            # You are calling one method of a class from another method of the same class.
+            preprocessor_obj = self.get_data_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score","reading_score"]
+
+            input_feature_train_df = train_df.drop(columns = [target_column_name],axis = 1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns = [target_column_name],axis = 1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(
+                "Applying the preprocessing object on training dataframe and testing dataframe"
+            )
+
+            # Applying the preprocessing on tain and test     
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+
+            #np.c_ means:Colum -wisr concatenation (join array side by side) X(features)  y(target)   after np.c_  [X(features),y(target)]
+            #model now gets inputs + target combined in one array.
+
+            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+
+            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object")
+
+            # preprocessing object need to be converted into pickle file . we have the path and save this pickle file in same path
+            # we have written this function into utils.
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessor_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+                #Accessing a variable stored inside another object which is stored inside your object
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
